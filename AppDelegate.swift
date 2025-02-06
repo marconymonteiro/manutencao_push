511df1929257b9dc702eab21a6c970a233020feb
marconymonteiro <65182991+marconymonteiro@users.noreply.github.com>
2025-02-05 21:00:09 -0300

    Create AppDelegate.swift

---
+++ AppDelegate.swift
@@ -0,0 +1,93 @@
+import UIKit
+import FirebaseCore
+import FirebaseMessaging
+
+@main
+@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+
+  let gcmMessageIDKey = "gcm.Message_ID"
+
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    FirebaseApp.configure()
+
+    // PUSH NOTIFICATION START
+
+    UNUserNotificationCenter.current().delegate = self
+
+    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+    UNUserNotificationCenter.current().requestAuthorization(
+      options: authOptions,
+      completionHandler: { _, _ in }
+    )
+
+    application.registerForRemoteNotifications()
+
+    // MESSAGING DELEGATE
+
+    Messaging.messaging().delegate = self
+
+    // PUSH NOTIFICATION END
+
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  // RECEIVE NOTIFICATION IN IOS
+
+  // Método para receber notificações em primeiro plano
+  override func userNotificationCenter(
+    _ center: UNUserNotificationCenter,
+    willPresent notification: UNNotification,
+    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+  ) {
+    let userInfo = notification.request.content.userInfo
+    print(userInfo)
+    completionHandler([[.alert, .sound]])
+  }
+
+  // Método para lidar com toques em notificações
+  override func userNotificationCenter(
+    _ center: UNUserNotificationCenter,
+    didReceive response: UNNotificationResponse,
+    withCompletionHandler completionHandler: @escaping () -> Void
+  ) {
+    let userInfo = response.notification.request.content.userInfo
+    print(userInfo)
+    completionHandler()
+  }
+
+  // Método para receber notificações em segundo plano
+  override func application(
+    _ application: UIApplication,
+    didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+    fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+  ) {
+    if let messageID = userInfo[gcmMessageIDKey] {
+      print("Message ID: \(messageID)")
+    }
+
+    print(userInfo)
+
+    completionHandler(.newData)
+  }
+}
+
+// MESSAGING DELEGATE
+
+extension AppDelegate {
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    print("Firebase registration token: \(String(describing: fcmToken))")
+
+    let dataDict: [String: String] = ["token": fcmToken ?? ""]
+    NotificationCenter.default.post(
+      name: Notification.Name("FCMToken"),
+      object: nil,
+      userInfo: dataDict
+    )
+    // TODO: If necessary send token to application server.
+    // Note: This callback is fired at each app startup and whenever a new token is generated.
+  }
+}
